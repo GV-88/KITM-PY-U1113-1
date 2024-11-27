f5e69333116e6dcbba32d658ba71ccd5ba2aa29a
Gytis V <sherman.triplex@gmail.com>
2024-11-27 08:05:04 +0200

    lyg tai pabaigta dalis su spėjimu

--- 03_atsitiktiniai_ver02.py
+++ 03_atsitiktiniai_ver02.py
@@ -11,9 +11,9 @@ from random import randint
 def registruotiSpejima(spejimoNr, spejimas, sugeneruotoSkirtumas, failas):
 	if(failas):
 		if sugeneruotoSkirtumas == 0:
-			print(f'{spejimoNr} spėjimu vartotojas atspėjo skaičių')
+			print(f'{spejimoNr} spėjimu vartotojas atspėjo skaičių', file=failas)
 		else:
-			print(f'{spejimoNr} spėjimu vartotojas įvedė {spejimas}. Atsakymas - sugeneruotas skaičius {"didesnis" if sugeneruotoSkirtumas > 0 else "mažesnis"}')
+			print(f'{spejimoNr} spėjimu vartotojas įvedė {spejimas}. Atsakymas - sugeneruotas skaičius {"didesnis" if sugeneruotoSkirtumas > 0 else "mažesnis"}', file=failas)
 
 
 def tikrintiBandyma(spejimoNr, spejimas, atsakymas, nuo, iki, failas):
@@ -28,38 +28,37 @@ def tikrintiBandyma(spejimoNr, spejimas, atsakymas, nuo, iki, failas):
 		print(f'Sugeneruotas skaičius mažesnis už {spejimas}.', end=' ')
 		registruotiSpejima(spejimoNr, spejimas, -1, failas)
 		return {'atspejo': False, 'tinkamasBandymas': True}
-	if(failas):
-		registruotiSpejima(spejimoNr, spejimas, 1, failas)
-	return {'atspejo': True, 'tinkamasBandymas': False}
+	registruotiSpejima(spejimoNr, spejimas, 0, failas)
+	return {'atspejo': True, 'tinkamasBandymas': True}
 
 kiekZaidimu = 0
 zaidziam = True
 failas = 'reg.txt'
 
 with open(failas, 'a+', encoding='utf8') as f:
-	print('--------------------------------', file=failas)
+	print('--------------------------------', file=f)
 	while zaidziam:
 		n = 0
 		kiekSpejimu = kiekNetinkamu = 0
 		while n <= 1:
 			n = int(input('Nuo 1 iki kiek?...'))
-		print(f'Vartotojas įvedė skaičių {n}.', file=failas)
+		print(f'Vartotojas įvedė skaičių {n}.', file=f)
 		spejimas = 0
 		atsakymas = randint(1, n)
-		print(f'Sugeneruotas atsitiktinis skaičius {n}.', file=failas)
+		print(f'Sugeneruotas atsitiktinis skaičius {atsakymas}.', file=f)
 		print(f'Atspėkite skaičių nuo 1 iki {n}.', end=(' '))
 		# vidinis ciklas; būtų galima tikrinti spejimas==atsakymas, bet jeigu tai resursų reikalaujantis tikrinimas, nėra reikalo tikrinti su kiekvienu nereikšmingu bandymu
 		while zaidziam:
 			spejimas = int(input(f'Atliksite {kiekSpejimu+1} spėjimą...'))
 
-			tarpinisRezultatas = tikrintiBandyma(kiekSpejimu+1, spejimas, atsakymas, 1, n, failas)
+			tarpinisRezultatas = tikrintiBandyma(kiekSpejimu+1, spejimas, atsakymas, 1, n, f)
 
 			if tarpinisRezultatas['tinkamasBandymas']:
 				kiekSpejimu += 1
 			else:
 				kiekNetinkamu += 1
 				continue
-			if not tarpinisRezultatas['atpejo']:
+			if not tarpinisRezultatas['atspejo']:
 				continue
 
 			# čia logiškai turėtų atitikti spejimas==atsakymas
@@ -72,10 +71,10 @@ with open(failas, 'a+', encoding='utf8') as f:
 				match(dar):
 					case 't' | 'T':
 						zaidziam = True
-						print(f'Į užklausą „Ar žaisite dar“ pasirinko „Taip“', file=failas)
+						print(f'Į užklausą „Ar žaisite dar“ pasirinko „Taip“', file=f)
 					case 'n' | 'N':
 						zaidziam = False
-						print(f'Į užklausą „Ar žaisite dar“ pasirinko „Ne“', file=failas)
+						print(f'Į užklausą „Ar žaisite dar“ pasirinko „Ne“', file=f)
 					case _: dar = '?'
 			break # išeiname iš vidinio ciklo su atnaujinta reikšme zaidziam
-	print(f'Vartotojas žaidė {kiekZaidimu} kartus(ų)', file=failas)
+	print(f'Vartotojas žaidė {kiekZaidimu} kartus(ų)', file=f)
